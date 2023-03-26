865d523c89e67da151fb290527a13261ed345096
AkiraDemenech <guiakira.demenechmori@gmail.com>
2023-03-26 13:36:30 -0300

    União de Vítimas e Ocorrências

--- mjsp.br.py
+++ mjsp.br.py
@@ -1,4 +1,18 @@
 first_number = lambda s, end=1: ((first_number(s, end + 1) if end < len(s) and s[end].isdigit() else int(s[:end])) if s[0].isdigit() else first_number(s[1:])) if len(s) else None 
+months = {
+	'janeiro':	1,
+	'fevereiro':	2,
+	'março':	3,
+	'abril':	4,
+	'maio':	5,
+	'junho':	6,
+	'julho':	7,
+	'agosto':	8,
+	'setembro':	9,
+	'outubro':	10,
+	'novembro':	11,
+	'dezembro':	12
+	}
 
 #import xlrd
 import pandas 
@@ -79,22 +93,46 @@ def seg_pub (arq = './dados/indicadoressegurancapublicauf (1).xls', ibge_pop = N
 	if ibge_pop == None:
 		ibge_pop = ibge()[0]
 
-	seg_pub = []
+	seg_pub = {}
 	seg_pub_xls = pandas.ExcelFile(arq)
 	ocorr = pandas.read_excel(seg_pub_xls, 'Ocorrências')
 	vitim = pandas.read_excel(seg_pub_xls, 'Vítimas')
 
 	for i in range(len(ocorr)):
+		crime = ocorr[i]['Tipo Crime']
 		ano = ocorr[i]['Ano']
+		uf = ocorr[i]['UF']
 		ln = dict(ibge_pop[ano])
-		ln['UF'] = ocorr[i]['UF']
-		ln['Crime'] = ocorr[i]['Tipo Crime']
-		ln['Sexo'] = 'Total'
+		
+		if not uf in seg_pub:
+			seg_pub[uf] = {}
+		if not crime in seg_pub[uf]:
+			seg_pub[uf][crime] = {}
+		seg_pub[uf][crime]['Total'] = ln	
+
+	#	ln['UF'] = ocorr[i]['UF']
+	#	ln['Crime'] = ocorr[i]['Tipo Crime']
+	#	ln['Sexo'] = 'Total'
 		
 		ln['Ano'] = ano
-		ln['Mês'] = ocorr[i]['Mês']
-		ln['Vítimas'] = ocorr[i]['Vítimas']
-		seg_pub.append(ln)
+		ln['Mês'] = months(ocorr[i]['Mês'].strip().lower())
+		ln['Ocorrências'] = ocorr[i]['Ocorrências']
+
+	for i in range(len(vitim)):	
+		crime = vitim[i]['Tipo Crime']
+		ano = vitim[i]['Ano']
+		uf = vitim[i]['UF']
+		ln = dict(ibge_pop[ano])
+		
+		if not uf in seg_pub:
+			seg_pub[uf] = {}
+		if not crime in seg_pub[uf]:
+			seg_pub[uf][crime] = {}
+		seg_pub[uf][crime][vitim[i]['Sexo da Vítima']] = ln
+
+		ln['Ano'] = ano
+		ln['Mês'] = months(vitim[i]['Mês'].strip().lower())
+		ln['Ocorrências'] = vitim[i]['Vítimas']
 		
 	return seg_pub
 		
