5e973c31aa030302b5f85c78fbba2a4f77dd52e4
AkiraDemenech <guiakira.demenechmori@gmail.com>
2023-03-26 12:45:00 -0300

    Leitura dos dados do IBGE

--- mjsp.br.py
+++ mjsp.br.py
@@ -8,26 +8,70 @@ import os
 timestamp = lambda lt = None: timestamp(time.localtime()) if lt == None else (lt[2::-1] + lt[3:6])
 s_timestamp = lambda t = None: s_timestamp(timestamp()) if t == None else '%02d/%02d/%d %02d:%02d:%02d' %t
 
-caminho = './dados/'
+def ibge (caminho = './dados/'):
 
-ibge = {}
+	ibge = {}
+	raw_ibge = {}
+	
 
-for arq in os.listdir(caminho):
-	if arq.strip().lower().endswith('.xls'):	
 		
-		ano = first_number(arq)
-		print('\n' + s_timestamp() + '\t', ano, '\t', arq)
+	for arq in os.listdir(caminho):
+		if arq.strip().lower().endswith('.xls'):	
+			
+			ano = first_number(arq)
+			print('\n' + s_timestamp() + '\t', ano, '\t', arq)
 
-		if ano == None or ano < 1000:
-			continue
+			if ano == None or ano < 1000:
+				continue
 
-		print(s_timestamp(), '\tReading file....')	
+			print(s_timestamp(), '\tReading file....')	
 
-		i = time.time_ns()
-		ibge[ano] = pandas.read_excel(caminho + arq) 		
-		f = time.time_ns()
+			i = time.time_ns()
+			raw_ibge[ano] = pandas.read_excel(caminho + arq) 		
+			f = time.time_ns()
 
-		print(s_timestamp(), '\t',len(ibge[ano]), 'raw rows in',(f-i)/1000000,'ms')
+			print(s_timestamp(), '\t',len(raw_ibge[ano]), 'raw rows in',(f-i)/1000000,'ms')
+
+			ibge[ano] = {}
+			uf_col = pop_col = []
+
+			for col in set(raw_ibge[ano]):
+				t = n = 0
+				dados = []
+				for i in range(len(raw_ibge[ano][col])):
+					if type(raw_ibge[ano][col][i]) == str:
+						t += 1
+					elif type(raw_ibge[ano][col][i]) == int:
+						n += 1
+					dados.append(raw_ibge[ano][col][i])	
+
+				if t > n: 	
+					print(s_timestamp(), '\tUF:', repr(col))
+					uf_col = dados
+				elif n > 0 and t > 0:	
+					print(s_timestamp(), '\tEstimativa:', repr(col))
+					pop_col = [(first_number(valor.strip().replace('.','')) if type(valor) == str else valor) for valor in dados]
+				 									
+
+			for i in range(len(pop_col)):	
+				if pop_col[i] == None or type(pop_col[i]) == float or type(uf_col[i]) == float:
+					continue 
+
+				
+
+				ibge[ano][uf_col[i].strip()] = pop_col[i]
+
+			
+
+
+	return ibge, raw_ibge		
+
+a = ibge()[0]
+for k in a:
+	print('\n', k)
+	for uf in a[k]:
+		print(uf,'\t', a[k][uf])
+
 
 #exit()
 pop2020 = pandas.read_excel('IA/aprendizado de máquina/estimativa_dou_2020.xls')
